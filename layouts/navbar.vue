e7fa01df6ba374cbcb9ecfe47a10556a0bbcbfe7
jasonkiptoo <kiptoojason@gmail.com>
2024-03-22 11:21:39 +0300

    new config for portfolio

---
+++ layouts/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- <div class="containr mx-auto"> -->
+       <header class="containr w-full  mx-auto p-5 shadow-sm bg-white">
+        <nav class="container mx-auto" >
+            <NuxtLink class=" flex justify-between">
+                <NuxtLink to="/" class="font-bold text-2xl">Jason Kiptoo 🐝</NuxtLink>
+            <ul class="flex  gap-4">
+            <li>About</li>
+            <li>Experience</li>
+            <li>Projects</li>
+            <li>Contact me</li>
+        </ul>
+            </NuxtLink>
+
+        </nav>
+       </header>
+
+       <div class="container mx-auto mt-4">
+            <slot />
+        </div>
+       
+    <!-- </div> -->
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.router-link-exact-active{
+    color: #a32a29;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="ard flex justify-around">
+
+        <div class="image">
+            <!-- <NuxtImg src="/nuxt-icon.png" /> -->
+            <!-- <img src="~/assets/images/DSC_0567.JPG" alt="Jason Kiptoo"> -->
+
+        </div>
+
+        <div class="about">
+            About me
+        </div>
+
+
+      
+
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'navbar'
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
