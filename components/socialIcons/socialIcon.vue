d63d1591799c6a2ce76a4b1f2b2eafb9076da5ff
jasonkiptoo <kiptoojason@gmail.com>
2024-03-22 12:31:43 +0300

    update to add icons

---
+++ components/socialIcons/socialIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+        LinkedIn
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,60 @@
 <template>
-    <div class="flex justify-center gap-12">
-
-        <div class="image content-center">
-            <img class="rounded-full content-center" width="500px" height="400px"  src="https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Jason Kiptoo">
-        </div>
-
-        <div class=" about content-center text-center ">
-
-            <h5 class="text-3xl font-mono" >Hello, I'm</h5>
-            <h3 class="font-bold text-5xl">Jason Kiptoo  </h3>
-            <h2 class="font-mono text-3xl">Frontend Developer</h2>
-
+    <div class="car flex justify-center gap-20">
+      <div class="image content-center mt-4">
+        <img
+          class=" content-center"
+        
+          src="~/assets/images/jay1.jpg"
+          alt="Jason Kiptoo"
+        >
+      </div>
+  
+      <div class="about content-center text-center">
+        <h5 class="text-3xl font-mono">Hello, I'm</h5>
+        <h3 class="font-bold text-5xl">Jason Kiptoo</h3>
+        <h2 class="font-mono text-3xl">Frontend Developer</h2>
+  
         <div class="buttons flex justify-around">
-            <div class="btns">
-
-Download CV
-</div>
-<div class="btns">
-    Contact me
-</div>
+          <div class="btns">Download CV</div>
+          <div class="btns">Contact me</div>
         </div>
-
-
+        <div class="socials flex gap-3 justify-center p-3">
+            <i class="fa-brands fa-github"></i>
+            <SocialIconsSocialIcon/>
+            <SocialIconsSocialIcon/>
         </div>
-
-
-      
-
+      </div>
     </div>
-    
-</template>
-
-<script setup>
-definePageMeta({
+  </template>
+  
+  <script setup>
+  definePageMeta({
     layout: 'navbar'
-})
-
-</script>
-
-<style scoped>
-.image{
+  })
+  </script>
+  
+  <style scoped>
+  .image {
     /* background-color: red; */
-}
-.about{
+  }
+  
+  .about {
     /* background-color: purple; */
-}
-
-</style>
+  }
+  img{
+    border-radius: 80%;
+    height: 500px;
+    width: 500px;
+  }
+  
+  /* .btns {
+    @apply bg-[#a32a29] text-white px-3 py-2 rounded-full text-sm cursor-pointer border border-[#a32a29];
+    transition: background-color 0.3s ease;
+  
+    &:hover {
+      background-color: white;
+      color: #a32a29;
+    }
+  } */
+  </style>
+  
