1a9e3af446d5c00838cdf2a9ff7126b883911eb7
jasonkiptoo <kiptoojason@gmail.com>
2024-03-22 11:47:01 +0300

    Home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="ard flex justify-around">
+    <div class="flex justify-center gap-12">
 
         <div class="image">
-            <!-- <NuxtImg src="/nuxt-icon.png" /> -->
-            <!-- <img src="~/assets/images/DSC_0567.JPG" alt="Jason Kiptoo"> -->
-
+            <img class="rounded-full" width="500px" height="400px"  src="https://plus.unsplash.com/premium_photo-1675034393381-7e246fc40755?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Jason Kiptoo">
         </div>
 
-        <div class="about">
-            About me
+        <div class=" about content-center text-center ">
+
+            <h5>Hello, I'm</h5>
+            <h3 class="font-bold text-5xl">Jason Kiptoo  </h3>
+            <h2 class="font-mono text-3xl">Frontend Developer</h2>
+
         </div>
 
 
@@ -24,6 +26,12 @@ definePageMeta({
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.image{
+    /* background-color: red; */
+}
+.about{
+    /* background-color: purple; */
+}
 
 </style>
