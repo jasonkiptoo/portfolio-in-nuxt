f59ef3ae05d75cef3fd91a1fc918104b38213794
jasonkiptoo <kiptoojason@gmail.com>
2024-03-25 05:16:16 +0300

    new fix to responsivenes

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -2,14 +2,14 @@
     <header class="containe w-full sticky top-0 mx-auto p-5 shadow-sm bg-white">
       <nav class="container mx-auto flex flex-wrap justify-between">
         <NuxtLink to="/" class="font-bold text-2xl">Jason Kiptoo 🐝</NuxtLink>
-        <ul class="flex gap-8 nav" :class="{ 'hidden': isMenuOpen }">
+        <ul class="flex gap-8 nav" v-bind:class="{ 'hidden': isMenuOpen }">
           <li><NuxtLink to="/" class="">Home</NuxtLink></li>
           <li><NuxtLink to="/about" class="font-bold text-2x">About</NuxtLink></li>
           <li><NuxtLink to="/experience" class="font-bold text-2x">Experience</NuxtLink></li>
           <li><NuxtLink to="/projects" class="font-bold text-2x">Projects</NuxtLink></li>
           <li><NuxtLink to="/contact" class="font-bold text-2x">Contact me</NuxtLink></li>
         </ul>
-        <div class="menu-icon" @click="toggleMenu()">
+        <div class="menu-icon" @click="isMenuOpen = !isMenuOpen">
           <Icon size="40px" color="#a32a29" name="material-symbols:lists-rounded"/>
         </div>
       </nav>
@@ -27,7 +27,7 @@
   
   const toggleMenu = () => {
     console.log("click");
-    isMenuOpen.value = !isMenuOpen.value;
+    isMenuOpen.value = false;
   };
   </script>
   
@@ -52,10 +52,11 @@
         display: flex;
         flex-direction: column;
         position:absolute;
-        right: 10px;
+        left: 10px;
         top: 50px;
-        float: right-inherit;
-      /* display: none; */
+        background-color: white;
+        padding: 20px;
+      display: none;
     }
   
     .menu-icon {
